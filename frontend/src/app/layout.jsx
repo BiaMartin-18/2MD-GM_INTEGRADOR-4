ce526df5c1644eeb78434c9bb5fdd32b87298f71
BEATRIZ MARTIN DA PAZ <25170075@educ123.sp.senai.br>
2025-11-12 08:19:27 -0300

    Alteração Nabvar 2

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-     <Navbar />
         {children}
         <BootstrapClient />
         <Footer />
