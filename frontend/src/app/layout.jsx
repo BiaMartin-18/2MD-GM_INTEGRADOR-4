009e69f3e54663b3a30a8605ce307f1c80d0890f
ANA KAROLINE OLIVEIRA NOVAES <25170072@educ123.sp.senai.br>
2025-11-12 11:05:18 -0300

    Pequena alteração layout

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -25,6 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <BootstrapClient />
