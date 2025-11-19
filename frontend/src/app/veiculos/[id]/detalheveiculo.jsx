628d929dfbc75cac4fa449a3054fa44d5411f50d
ANA KAROLINE OLIVEIRA NOVAES <25170072@educ123.sp.senai.br>
2025-11-19 10:40:41 -0300

    Alteração página de veículos e páginas de [id]

---
+++ frontend/src/app/veiculos/[id]/detalheveiculo.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useEfect, useState } from "react";
+import "./veiculo.css"
+
+export default function DetalhesVeiculos({ vehicleId }) {
+    const [veiculo, setVeiculo] = useState(null)
+
+    useEffect(() => {
+        if (vehicleId) {
+            
+        }
+    })
+}
