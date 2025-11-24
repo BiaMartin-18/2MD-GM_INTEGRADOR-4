402e2a36a8563da9770fe98add05ef5f49317a10
PALOMA DE SOUZA VICENTE <25170405@educ123.sp.senai.br>
2025-11-24 07:47:15 -0300

    Alterações

--- frontend/src/app/suporte/page.jsx
+++ frontend/src/app/suporte/page.jsx
@@ -19,12 +19,8 @@ const SuportePage = () => {
         <i className="fas fa-building contact-icon-axionet mb-3"></i>
         <h6 className="fw-bold">{city}</h6>
         <p className="small text-muted mb-1">{address}</p>
-        <p className="small text-muted mb-1">
-          **Tel:** {phone}
-        </p>
-        <p className="small text-muted mb-3">
-          **Fax:** {fax}
-        </p>
+        <p className="small text-muted mb-1">**Tel:** {phone}</p>
+        <p className="small text-muted mb-3">**Fax:** {fax}</p>
         <a href="#" className="view-directions-axionet">
           Ver Localização <i className="fas fa-arrow-right"></i>
         </a>
@@ -37,18 +33,21 @@ const SuportePage = () => {
       <Navbar />
       <main className="suporte-wrapper-content">
         <header className="hero-support-axionet">
-         
           <div className="container py-5">
             <div className="row">
               <div className="col-lg-12">
                 <h1 className="hero-title-axionet mb-3">
                   <span className="text-blue">Fale com</span>
                   <br />
-                  <span className="text-accent-axionet">Nossa Equipe de Suporte</span>
+                  <span className="text-accent-axionet">
+                    Nossa Equipe de Suporte
+                  </span>
                 </h1>
                 <p className="hero-lead-axionet">
-                  Estamos aqui para ajudar você a maximizar a eficiência do Controle PDI. Descreva o problema ou
-                  necessidade para que nosso especialista no gerenciamento de defeitos e custos possa auxiliar rapidamente.
+                  Estamos aqui para ajudar você a maximizar a eficiência do
+                  Controle PDI. Descreva o problema ou necessidade para que
+                  nosso especialista no gerenciamento de defeitos e custos possa
+                  auxiliar rapidamente.
                 </p>
               </div>
             </div>
@@ -59,8 +58,10 @@ const SuportePage = () => {
         <section className="py-5">
           <div className="container">
             {/* Título "Connect with a Specialist" */}
-            <h2 className="section-title-specialist-axionet mb-5">
-              Conecte-se <span className="text-accent-axionet">com um Especialista</span>
+            <h2 className="section-title-specialist-axionet mb-5 text-center">
+              Conecte-se  
+              <span className="text-accent-axionet">
+              </span>
             </h2>
 
             <div className="row">
@@ -109,37 +110,106 @@ const SuportePage = () => {
                     <div className="row g-3">
                       {/* Primeira Linha: Nome e Sobrenome */}
                       <div className="col-md-6">
-                        <label htmlFor="firstName" className="form-label small text-muted">Primeiro Nome*</label>
-                        <input type="text" className="form-control" id="firstName" placeholder="Seu Primeiro Nome" required />
+                        <label
+                          htmlFor="firstName"
+                          className="form-label small text-muted"
+                        >
+                          Primeiro Nome*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="firstName"
+                          placeholder="Seu Primeiro Nome"
+                          required
+                        />
                       </div>
                       <div className="col-md-6">
-                        <label htmlFor="lastName" className="form-label small text-muted">Sobrenome*</label>
-                        <input type="text" className="form-control" id="lastName" placeholder="Seu Sobrenome" required />
+                        <label
+                          htmlFor="lastName"
+                          className="form-label small text-muted"
+                        >
+                          Sobrenome*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="lastName"
+                          placeholder="Seu Sobrenome"
+                          required
+                        />
                       </div>
 
                       {/* Segunda Linha: E-mail e Telefone */}
                       <div className="col-md-6">
-                        <label htmlFor="emailAddress" className="form-label small text-muted">E-mail Corporativo*</label>
-                        <input type="email" className="form-control" id="emailAddress" placeholder="email@gm.com" required />
+                        <label
+                          htmlFor="emailAddress"
+                          className="form-label small text-muted"
+                        >
+                          E-mail Corporativo*
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="emailAddress"
+                          placeholder="email@gm.com"
+                          required
+                        />
                       </div>
                       <div className="col-md-6">
-                        <label htmlFor="phone" className="form-label small text-muted">Telefone</label>
-                        <input type="tel" className="form-control" id="phone" placeholder="(XX) XXXX-XXXX" />
+                        <label
+                          htmlFor="phone"
+                          className="form-label small text-muted"
+                        >
+                          Telefone
+                        </label>
+                        <input
+                          type="tel"
+                          className="form-control"
+                          id="phone"
+                          placeholder="(XX) XXXX-XXXX"
+                        />
                       </div>
 
                       {/* Terceira Linha: Área/Setor e Cargo */}
                       <div className="col-md-6">
-                        <label htmlFor="companyName" className="form-label small text-muted">Nome da Área/Setor*</label>
-                        <input type="text" className="form-control" id="companyName" placeholder="Ex: Engenharia de Qualidade" required />
+                        <label
+                          htmlFor="companyName"
+                          className="form-label small text-muted"
+                        >
+                          Nome da Área/Setor*
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="companyName"
+                          placeholder="Ex: Engenharia de Qualidade"
+                          required
+                        />
                       </div>
                       <div className="col-md-6">
-                        <label htmlFor="jobTitle" className="form-label small text-muted">Cargo</label>
-                        <input type="text" className="form-control" id="jobTitle" placeholder="Ex: Supervisor de Manutenção" />
+                        <label
+                          htmlFor="jobTitle"
+                          className="form-label small text-muted"
+                        >
+                          Cargo
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="jobTitle"
+                          placeholder="Ex: Supervisor de Manutenção"
+                        />
                       </div>
 
                       {/* Quarta Linha: Modelo de Veículo e Tipo de Solicitação (Selects) */}
                       <div className="col-md-6">
-                        <label htmlFor="vehicleModel" className="form-label small text-muted">Modelo de Veículo</label>
+                        <label
+                          htmlFor="vehicleModel"
+                          className="form-label small text-muted"
+                        >
+                          Modelo de Veículo
+                        </label>
                         <select id="vehicleModel" className="form-select">
                           <option defaultValue>Selecione o Modelo</option>
                           <option>Caminhonete K5</option>
@@ -148,7 +218,12 @@ const SuportePage = () => {
                         </select>
                       </div>
                       <div className="col-md-6">
-                        <label htmlFor="issueType" className="form-label small text-muted">Tipo de Solicitação*</label>
+                        <label
+                          htmlFor="issueType"
+                          className="form-label small text-muted"
+                        >
+                          Tipo de Solicitação*
+                        </label>
                         <select id="issueType" className="form-select" required>
                           <option defaultValue>Selecione o Tipo</option>
                           <option>Erro no Sistema (Bug)</option>
@@ -160,22 +235,51 @@ const SuportePage = () => {
 
                       {/* Quinta Linha: Mensagem */}
                       <div className="col-12">
-                        <label htmlFor="message" className="form-label small text-muted">Mensagem</label>
-                        <textarea className="form-control" id="message" rows="4" placeholder="Descreva seu problema com detalhes, mencionando placas ou códigos de erro, se houver." required></textarea>
+                        <label
+                          htmlFor="message"
+                          className="form-label small text-muted"
+                        >
+                          Mensagem
+                        </label>
+                        <textarea
+                          className="form-control"
+                          id="message"
+                          rows="4"
+                          placeholder="Descreva seu problema com detalhes, mencionando placas ou códigos de erro, se houver."
+                          required
+                        ></textarea>
                       </div>
 
                       {/* Sexta Linha: Checkbox e Botão */}
                       <div className="col-12 mt-3">
                         <div className="form-check">
-                          <input className="form-check-input" type="checkbox" id="privacyCheck" required />
-                          <label className="form-check-label small text-muted" htmlFor="privacyCheck">
-                            Ao enviar, concordo com o processamento dos dados conforme a
-                            <a href="#" className="text-decoration-none text-accent-axionet"> Política de Privacidade</a>
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="privacyCheck"
+                            required
+                          />
+                          <label
+                            className="form-check-label small text-muted"
+                            htmlFor="privacyCheck"
+                          >
+                            Ao enviar, concordo com o processamento dos dados
+                            conforme a
+                            <a
+                              href="#"
+                              className="text-decoration-none text-accent-axionet"
+                            >
+                              {" "}
+                              Política de Privacidade
+                            </a>
                           </label>
                         </div>
                       </div>
                       <div className="col-12 mt-4">
-                        <button type="submit" className="btn btn-lg w-100 btn-submit-axionet">
+                        <button
+                          type="submit"
+                          className="btn btn-lg w-100 btn-submit-axionet"
+                        >
                           Enviar Solicitação
                         </button>
                       </div>
