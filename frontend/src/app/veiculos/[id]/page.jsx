31480c17db38352f51d3f66561244882e81c608e
ANA KAROLINE OLIVEIRA NOVAES <25170072@educ123.sp.senai.br>
2025-11-26 10:55:56 -0300

    Alterações page [id]

--- frontend/src/app/veiculos/[id]/page.jsx
+++
@@ -1,6 +0,0 @@
-import DetalhesVeiculo from "./detalheveiculo";
-
-export default async function VeiculoDetails({ params }) {
-  const { id } = await params; // Next 16 exige isso
-  return <DetalhesVeiculo vehicleId={id} />;
-}
