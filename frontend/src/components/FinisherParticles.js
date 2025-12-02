13c44b3faa434c49019419b62481c9dc39d3a025
ANA KAROLINE OLIVEIRA NOVAES <25170072@educ123.sp.senai.br>
2025-12-02 10:24:17 -0300

    Alterações fundo dinâmico

--- frontend/src/components/FinisherParticles.js
+++ frontend/src/components/FinisherParticles.js
@@ -12,30 +12,27 @@ export default function FinisherParticles() {
     script.src = "/finisher-header.es5.min.js";
 
     script.onload = () => {
-  if (window.FinisherHeader) {
-    // garante que a hero já existe no DOM
-    requestAnimationFrame(() => {
-      new window.FinisherHeader({
-        className: "finisher-header",
-        count: 6,
-        size: { min: 1100, max: 1300, pulse: 0 },
-        speed: { 
-          x: { min: 0.1, max: 0.8 }, 
-          y: { min: 0.1, max: 0.8 }
-        },
-        colors: {
-          background: "rgba(69, 118, 255, 1)",
-          particles: ["#077eedff", "#091061", "#006eb5"],
-        },
-        blending: "overlay",
-        opacity: { center: 1, edge: 0.1 },
-        skew: 0,
-        shapes: ["c"],
-      });
-    });
-  }
-};
-
+      // O script carregou → agora podemos inicializar
+      if (window.FinisherHeader) {
+        new window.FinisherHeader({
+          className: "finisher-header",
+          count: 6,
+          size: { min: 1100, max: 1300, pulse: 0 },
+          speed: {
+            x: { min: 0.1, max: 0.8 },
+            y: { min: 0.1, max: 0.8 },
+          },
+          colors: {
+            background: "rgba(69, 118, 255, 1)",
+            particles: ["#077eedff", "#091061", "#006eb5"],
+          },
+          blending: "overlay",
+          opacity: { center: 1, edge: 0.1 },
+          skew: 0,
+          shapes: ["c"],
+        });
+      }
+    };
 
     script.onerror = () => {
       console.error("Erro ao carregar Finisher Header");
@@ -46,7 +43,18 @@ export default function FinisherParticles() {
   return (
     <div
       ref={containerRef}
-      className="finisher-container"
+      className="finisher-header"
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: "20px",
+        margin: 0,
+        zIndex: 1,
+        pointerEvents: "none",
+      }}
     />
   );
 }
